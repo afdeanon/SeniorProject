2a571bc2f5455c8ab4b4d81b104a13be72ad5730
Angeli Faith Deanon <angelifaithdeanon@Angelis-MacBook-Pro-2.local>
2024-04-26 16:59:09 -0700

    Created Date and day list

--- lib/screens/workoutScreens/workoutScreen.dart
+++ lib/screens/workoutScreens/workoutScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_workout_app/screens/workoutScreens/horizontalDayListState.dart';
 
 class WorkoutScreen extends StatefulWidget {
   const WorkoutScreen({Key? key, User? user}) : super(key: key);
@@ -11,14 +12,33 @@ class WorkoutScreen extends StatefulWidget {
 class _WorkoutScreenState extends State<WorkoutScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Center(
       child: Container(
         child: Column(children: [
           Container(
-            width: 393,
-            height: 237,
+            padding: const EdgeInsets.fromLTRB(10, 6, 10, 10),
+            alignment: Alignment.centerLeft,
+            width: screenWidth, //screen width
             decoration: BoxDecoration(color: Color(0xFF558CE0)),
-          )
+            child: const Column(
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'April 2024',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontFamily: 'SF Pro',
+                    fontWeight: FontWeight.bold,
+                    height: 0,
+                  ),
+                ),
+                SizedBox(height: 10),
+                HorizontalDayList(),
+              ],
+            ),
+          ),
         ]),
       ),
     );
