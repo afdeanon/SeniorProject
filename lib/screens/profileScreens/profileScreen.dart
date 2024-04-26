e8254293c5abcb874dc426aa872a9011f7c28e05
Juhan Lee <soundy0115@gmail.com>
2024-04-25 18:27:43 -0700

    added profile picture and menu columns

--- lib/screens/profileScreens/profileScreen.dart
+++ lib/screens/profileScreens/profileScreen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_workout_app/image.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key, User? user}) : super(key: key);
@@ -13,7 +16,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+        scaffoldBackgroundColor: const Color(0xFFFFFF),
       ),
       home: Scaffold(
         body: ListView(children: [
@@ -35,94 +38,140 @@ class _ProfileState extends State<Profile> {
     return Column(
       children: [
         Container(
-          width: 393,
-          height: 852,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(color: Colors.white),
-          child: Stack(
+          width: 259,  // Consider whether you still need a fixed width here
+          height: 96,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                width: 330,
-                height: 418,
+                width: 96,
+                height: 96,
+                decoration: const ShapeDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(SImages.runningImage),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: OvalBorder(),
+                ),
+              ),
+              const SizedBox(width: 32),
+              const Expanded(  // This will allow the text container to resize dynamically
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      width: 330,
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                      decoration: ShapeDecoration(
-                        color: Color(0xFFF2F2F2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 240,
-                            child: Text(
-                              'Edit Profile',
-                              style: TextStyle(
-                                color: Color(0xFF343434),
-                                fontSize: 20,
-                                fontFamily: 'SF Pro',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 32),
-                        ],
+                    Text(
+                      'Joseph',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w500,
+                        height: 0,  // Check if 'height: 0' is necessary as it might cause layout issues
                       ),
                     ),
-                    const SizedBox(height: 12),
-                    Container(
-                      width: 330,
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                      decoration: ShapeDecoration(
-                        color: Color(0xFFF2F2F2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 240,
-                            child: Text(
-                              'My Workouts',
-                              style: TextStyle(
-                                color: Color(0xFF343434),
-                                fontSize: 20,
-                                fontFamily: 'SF Pro',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 32),
-                        ],
+                    Text(
+                      'Clark',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w500,
+                        height: 0,  // Check if 'height: 0' is necessary as it might cause layout issues
                       ),
                     ),
-                    const SizedBox(height: 12),
-                    Container(
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Center(
+          child: Container(
+            width: 330,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Your existing containers go here
+                Container(
+                  width: 330,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFFF2F2F2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 240,
+                        child: Text(
+                          'Edit Profile',
+                          style: TextStyle(
+                            color: Color(0xFF343434),
+                            fontSize: 20,
+                            fontFamily: 'SF Pro',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 32),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Container(
+                  width: 330,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFFF2F2F2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 240,
+                        child: Text(
+                          'My Workouts',
+                          style: TextStyle(
+                            color: Color(0xFF343434),
+                            fontSize: 20,
+                            fontFamily: 'SF Pro',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 32),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Container(
                       width: 330,
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
                       decoration: ShapeDecoration(
-                        color: Color(0xFFF2F2F2),
+                        color: const Color(0xFFF2F2F2),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15),
                         ),
                       ),
-                      child: Row(
+                      child: const Row(
                         mainAxisSize: MainAxisSize.min,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -140,7 +189,7 @@ class _ProfileState extends State<Profile> {
                               ),
                             ),
                           ),
-                          const SizedBox(width: 32),
+                          SizedBox(width: 32),
                         ],
                       ),
                     ),
@@ -149,12 +198,12 @@ class _ProfileState extends State<Profile> {
                       width: 330,
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
                       decoration: ShapeDecoration(
-                        color: Color(0xFFF2F2F2),
+                        color: const Color(0xFFF2F2F2),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15),
                         ),
                       ),
-                      child: Row(
+                      child: const Row(
                         mainAxisSize: MainAxisSize.min,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -172,7 +221,7 @@ class _ProfileState extends State<Profile> {
                               ),
                             ),
                           ),
-                          const SizedBox(width: 32),
+                          SizedBox(width: 32),
                         ],
                       ),
                     ),
@@ -181,12 +230,12 @@ class _ProfileState extends State<Profile> {
                       width: 330,
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
                       decoration: ShapeDecoration(
-                        color: Color(0xFFF2F2F2),
+                        color: const Color(0xFFF2F2F2),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15),
                         ),
                       ),
-                      child: Row(
+                      child: const Row(
                         mainAxisSize: MainAxisSize.min,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -204,16 +253,15 @@ class _ProfileState extends State<Profile> {
                               ),
                             ),
                           ),
-                          const SizedBox(width: 32),
+                          SizedBox(width: 32),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ],
+                // Continue with the remaining containers
+              ],
+            ),
           ),
-        ),
+        )
       ],
     );
   }
