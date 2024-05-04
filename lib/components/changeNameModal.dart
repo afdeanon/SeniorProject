ff46212da8e95f583f78f641f0e0043726999ccd
Juhan Lee <soundy0115@gmail.com>
2024-05-03 21:02:45 -0700

    finished the change name Modal

--- lib/components/changeNameModal.dart
+++ lib/components/changeNameModal.dart
@@ -1,197 +1,142 @@
 import 'package:flutter/material.dart';
 
-
 class ChangeNameModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 330,
-          height: 181,
-          clipBehavior: Clip.antiAlias,
-          decoration: ShapeDecoration(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 330,
-                height: 20,
-                decoration: const ShapeDecoration(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                ),
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      content: SizedBox(
+        width: 330,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Change Name',
+              style: TextStyle(
+                color: Color(0xFF343434),
+                fontSize: 20,
+                fontFamily: 'SF Pro',
+                fontWeight: FontWeight.w600,
               ),
-              Container(
-                width: 330,
-                height: 30,
-                decoration: const BoxDecoration(color: Colors.white),
-                child: const Row(
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Change Name',
+                    const Text(
+                      'First Name',
                       style: TextStyle(
-                        color: Color(0xFF343434),
-                        fontSize: 18,
+                        color: Colors.black,
+                        fontSize: 12,
                         fontFamily: 'SF Pro',
                         fontWeight: FontWeight.w600,
-                        height: 0,
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 330,
-                height: 12,
-                decoration: const BoxDecoration(color: Colors.white),
-              ),
-              Container(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
+                    const SizedBox(height: 5),
                     Container(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'First Name',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 12,
-                              fontFamily: 'SF Pro',
-                              fontWeight: FontWeight.w600,
-                              height: 0,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Container(
-                            width: 128,
-                            height: 36,
-                            decoration: ShapeDecoration(
-                              color: const Color(0xFFF2F2F2),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                        ],
+                      width: 128,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF2F2F2),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    Container(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Last Name',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 12,
-                              fontFamily: 'SF Pro',
-                              fontWeight: FontWeight.w600,
-                              height: 0,
-                            ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none, // Removes the underline
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 9), // Adjusts padding for vertical alignment
+                          hintText: 'Enter text', // Placeholder text
+                          hintStyle: TextStyle(
+                            fontSize: 12, // Font size for hint
                           ),
-                          const SizedBox(height: 5),
-                          Container(
-                            width: 128,
-                            height: 36,
-                            decoration: ShapeDecoration(
-                              color: const Color(0xFFF2F2F2),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 330,
-                height: 12,
-                decoration: const BoxDecoration(color: Colors.white),
-              ),
-              Container(
-                height: 32,
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: const BoxDecoration(color: Colors.white),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 7.50),
-                      decoration: ShapeDecoration(
-                        color: const Color(0xFF558CE0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
                         ),
+                        style: TextStyle(
+                          fontSize: 12, // Font size for input text
+                          fontFamily: 'SF Pro',
+                        ),
+                        cursorColor: Colors.black, // Sets the cursor color if needed
                       ),
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Save',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontFamily: 'SF Pro',
-                              fontWeight: FontWeight.w600,
-                              height: 0,
-                            ),
+                    ),
+
+                  ],
+                ),
+                const SizedBox(width: 7), // Space between the two columns (First Name and Last Name
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Last Name',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      width: 128,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF2F2F2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none, // Removes the underline
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 9), // Adjusts padding for vertical alignment
+                          hintText: 'Enter text', // Placeholder text
+                          hintStyle: TextStyle(
+                            fontSize: 12, // Font size for hint
                           ),
-                        ],
+                        ),
+                        style: TextStyle(
+                          fontSize: 12, // Font size for input text
+                          fontFamily: 'SF Pro',
+                        ),
+                        cursorColor: Colors.black, // Sets the cursor color if needed
                       ),
                     ),
                   ],
                 ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF558CE0), // Background color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 5), // Reduced padding
               ),
-              Container(
-                width: 330,
-                height: 20,
-                decoration: const ShapeDecoration(
+              onPressed: () {
+                // Add your functionality for what happens when the 'Save' button is clicked
+              },
+              child: const Text(
+                'Save',
+                style: TextStyle(
                   color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(60),
-                      bottomRight: Radius.circular(60),
-                    ),
-                  ),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ],
-          ),
+            ),
+
+          ],
         ),
-      ],
+      ),
     );
   }
 }
+
